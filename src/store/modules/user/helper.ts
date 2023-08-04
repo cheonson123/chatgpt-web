693bd68788409b9fbd88e1a47b80738f05521f0e
cheonson123 <100212419+cheonson123@users.noreply.github.com>
2023-08-04 12:32:16 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://images.cheonson.top/images/2023/04/11/_102027309_p1.jpg',
+      name: 'Cheonson',
+      description: 'Welcome to cheonson.top!',
     },
   }
 }
